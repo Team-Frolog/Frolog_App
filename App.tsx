8d8b3d42e3db022f4f131cb08165fc2c34331573
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-03-16 16:52:16 +0900

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/** @format */
+
+import { StyleSheet, View } from 'react-native';
+import WebView from 'react-native-webview';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WebView source={{ uri: 'https://www.frolog.kr' }} />
     </View>
   );
 }
@@ -13,8 +14,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
