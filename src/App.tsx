c00c739773a851e6701241ae0e570d1cf5e5a4d9
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-03-27 18:42:18 +0900

    [Feat/#4]: 페이지별 statusbar 스타일 동적 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import WebView from 'react-native-webview';
 
 export default function App() {
+  const [statusBarStyle, setStatusBarStyle] = useState<
+    'light' | 'dark' | 'auto'
+  >('light');
+
+  const handleChangeNavigation = (url: string) => {
+    if (url.includes('search') && !url.includes('search-home'))
+      return setStatusBarStyle('dark');
+    setStatusBarStyle('light');
+  };
+
   return (
     <View style={styles.container}>
-      <WebView source={{ uri: 'https://www.frolog.kr' }} />
+      <StatusBar style={statusBarStyle} />
+      <WebView
+        source={{ uri: 'https://www.frolog.kr' }}
+        onNavigationStateChange={(navState) =>
+          handleChangeNavigation(navState.url)
+        }
+      />
     </View>
   );
 }
