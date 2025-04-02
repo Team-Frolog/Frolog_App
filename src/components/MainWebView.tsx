18f4746b2959be643aaf5808cd61edd96d444e46
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-04-02 23:50:24 +0900

    [Feat/#4]: 건의하기 페이지 외부브라우저로 열기 구현

--- src/components/MainWebView.tsx
+++ src/components/MainWebView.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { StatusBar, StatusBarStyle } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { Linking, StyleSheet, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import WebView, { WebViewMessageEvent } from 'react-native-webview';
+import { ShouldStartLoadRequest } from 'react-native-webview/lib/WebViewTypes';
 import { THEME_COLOR } from '../constants/theme';
 
 function MainWebView() {
@@ -18,6 +19,15 @@ function MainWebView() {
 
     if (type === 'THEME' && THEME_COLOR[data]) setThemeState(THEME_COLOR[data]);
   };
+
+  const handleExternalPage = (req: ShouldStartLoadRequest) => {
+    if (req.url.includes('docs.google.com')) {
+      Linking.openURL(req.url);
+      return false;
+    }
+    return true;
+  };
+
   return (
     <>
       <StatusBar style={themeState.color} />
@@ -33,6 +43,7 @@ function MainWebView() {
         <WebView
           source={{ uri: 'https://frolog.kr' }}
           onMessage={(event) => handleMessage(event)}
+          onShouldStartLoadWithRequest={(req) => handleExternalPage(req)}
         />
       </View>
     </>
